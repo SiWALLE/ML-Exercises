0d082fcf622b360209c37bd2472c5737d4e6d2e8
SiWALLE <liangsibiao@live.com>
2015-10-16 21:54:58 +0800

    Network-backprop

--- src/Network.py
+++ src/Network.py
@@ -43,7 +43,39 @@ class Network(object):
         self.weights = [w-(eta/len(mini_batch))*nw for w,nw in zip(self.weights,nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb for b,nb in zip(self.biases,self.nabla_b)]      
     
-def sigmoid(self,z):
+    def backprop(self,x,y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        zs = []
+        for b,w in zip(self.biases,self.weights):
+            z = np.dot(w,activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activation[-1],y)*\
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta,activations[-2].transpose())
+        for l in xrange(2,self.num_layers):
+            z = zs[-1]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(),delta)*sp
+            nabla_b[-1] = delta
+            nabla_w[-1] = np.dot(delta,activation[-l-1].transpose())
+        return (nabla_b,nabla_w)
+
+    def evaluate(self,test_data):
+        test_results = [(np.argmax(self.feedforward(x)),y) for x,y in test_data]
+        return sum(int(x==y) for (x,y) in test_results)
+
+    def cost_derivative(self,output_activations,y):
+        return (output_activations-y)
+
+ def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
     
-    
+def sigmoid_prime(z):
+    temp = sigmoid(z)
+    return temp*(1-temp)
